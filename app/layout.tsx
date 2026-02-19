795fb9d75732073de8bb126557dc86dbe71cb8af
alokkkxpixel <alokpithale4@gmail.com>
2026-02-19 09:28:27 +0530

    meta icon removed

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,20 +20,20 @@ export const metadata: Metadata = {
   description: 'Your one-stop resource for finding syntax, documentation, and code snippets across all popular tech stacks.',
   generator: 'v0.app',
   icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
+    // icon: [
+    //   {
+    //     url: '/icon-light-32x32.png',
+    //     media: '(prefers-color-scheme: light)',
+    //   },
+    //   {
+    //     url: '/icon-dark-32x32.png',
+    //     media: '(prefers-color-scheme: dark)',
+    //   },
+    //   {
+    //     url: '/icon.svg',
+    //     type: 'image/svg+xml',
+    //   },
+    // ],
    
   },
 }
